6a03c451c0dbbf82c70cdac49fdda4c0303e8d9d
MadiGabrielle <marianafinamori@Marianas-MBP-2.lan1>
2019-06-06 22:58:56 -0400

    Updates

---
+++ precache-manifest.3a30ac38448d80589e4bf9052945aefb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf18d6c241a47caa6b5c19bb5ed6d50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a84f4f3de9acd5fb77f3",
+    "url": "/static/css/main.6461c065.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "a84f4f3de9acd5fb77f3",
+    "url": "/static/js/main.9d8c6e74.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
