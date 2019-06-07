07868504c3a68c6fc1e6c63bed5c8631039dd19f
MadiGabrielle <marianafinamori@dyn-160-39-194-20.dyn.columbia.edu>
2019-06-06 21:49:15 -0400

    run npm run build

---
+++ build/precache-manifest.86cdfae112ead0b59c7830a01387e90b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5e5547158c9a37b43300d59f9bb75c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51628f90ebbfd322483b",
+    "url": "/static/css/main.6461c065.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "51628f90ebbfd322483b",
+    "url": "/static/js/main.8a12ddc1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
